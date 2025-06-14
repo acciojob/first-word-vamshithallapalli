7593266158f995db35c1ec2d1f62d283f0755570
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-14 17:16:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
   // your code here
+  const arr = s.trim().split(" ");
+  return arr[0];
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
